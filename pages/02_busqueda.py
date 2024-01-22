980b7fa15ad9f7b0fc7de15d37f49652d3f090b6
rodrib <rodribogado50@gmail.com>
2024-01-22 17:30:51 -0300

    update busqueda.py

--- pages/02_busqueda.py
+++ pages/02_busqueda.py
@@ -16,21 +16,39 @@ panel_seleccionado = st.radio("Selecciona un Panel:", ["HC_38", "HC_55", "HC_117
 # Espacio en blanco para mostrar las columnas seleccionadas
 espacio_columnas = st.empty()
 
+# # Mostrar las columnas solo si se hizo clic en un panel
+# if st.button("Mostrar Genes del Panel"):
+#     # Filtrar el DataFrame para mostrar solo las filas correspondientes al panel seleccionado
+#     df_filtrado = df[df['Panel'] == panel_seleccionado]
+
+#     # Llenar el espacio en blanco con las filas y columnas seleccionadas
+#     espacio_columnas.write("Filas y Columnas Seleccionadas:")
+    
+#     # Mostrar panel y genes como enlaces y abrir GeneCards al hacer clic
+#     for index, row in df_filtrado.iterrows():
+#         panel = row["Panel"]
+#         genes = row["Genes-Panel"].split(", ")  # Separar los genes por comas
+#         espacio_columnas.write(f"Panel: {panel}")
+#         for gene_name in genes:
+#             gene_link = f"<a href='https://www.genecards.org/cgi-bin/carddisp.pl?gene={gene_name}&keywords={gene_name}'>{gene_name}</a>"
+#             st.markdown(gene_link, unsafe_allow_html=True)
+
 # Mostrar las columnas solo si se hizo clic en un panel
 if st.button("Mostrar Genes del Panel"):
     # Filtrar el DataFrame para mostrar solo las filas correspondientes al panel seleccionado
     df_filtrado = df[df['Panel'] == panel_seleccionado]
 
-    # Llenar el espacio en blanco con las filas y columnas seleccionadas
-    espacio_columnas.write("Filas y Columnas Seleccionadas:")
-    
-    # Mostrar panel y genes como enlaces y abrir GeneCards al hacer clic
-    for index, row in df_filtrado.iterrows():
-        panel = row["Panel"]
-        genes = row["Genes-Panel"].split(", ")  # Separar los genes por comas
-        espacio_columnas.write(f"Panel: {panel}")
-        for gene_name in genes:
-            gene_link = f"<a href='https://www.genecards.org/cgi-bin/carddisp.pl?gene={gene_name}&keywords={gene_name}'>{gene_name}</a>"
-            st.markdown(gene_link, unsafe_allow_html=True)
+    # Crear una lista de genes
+    genes = []
+    for _, row in df_filtrado.iterrows():
+        genes.extend(row["Genes-Panel"].split(", "))
 
+    # Crear una lista de enlaces de genes a GeneCards con formato HTML
+    enlaces_genes = []
+    for gene in genes:
+        gene_link = f"<a href='https://www.genecards.org/cgi-bin/carddisp.pl?gene={gene.replace(' ', '%20')}&keywords={gene.replace(' ', '%20')}' target='_blank'>{gene}</a>"
+        enlaces_genes.append(gene_link)
 
+    # Mostrar la lista de enlaces en formato HTML
+    espacio_columnas.write("Genes del Panel:")
+    espacio_columnas.markdown(", ".join(enlaces_genes), unsafe_allow_html=True)
